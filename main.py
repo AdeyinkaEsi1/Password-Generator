a721247c37fd1c0a965154798957a07811e1698c
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-02-14 21:25:16 +0100

    upd

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import random
 import string
 
 
-def get_password(length=12):
+def gen_pw(length=12):
     characters = string.ascii_letters + string.digits
     password = ''.join(random.choice(characters) for __ in range(length))
     return password
 
 
-password = get_password()
-print(f'newly generated password --> {password}')
+# password = gen_pw()
+# print(f'newly generated password --> {password}')
