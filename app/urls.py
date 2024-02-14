446f7ecc07029809975f6ed0c5d1951597ab91b5
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-02-14 21:27:31 +0100

    added paths

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import generate_password
+from . import views
+from django.contrib.auth.views import LoginView
+from .views import CustomLogoutView
 
 
 urlpatterns = [
-    path('', generate_password, name='generate_password')
+    path('', views.home, name='home'),
+    path('login', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', CustomLogoutView.as_view(), name='logout'),
+    path('register', views.register, name='register'),
+    path('generate_password', views.generate_password, name='generate_password'),
 ]
