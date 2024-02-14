57aa4bd9bd44b571d0578aa32a902a2d688d67d5
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-02-14 21:26:43 +0100

    added login, reg and User

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,22 @@
 from django import forms
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class Password_gen_form(forms.Form):
     length = forms.IntegerField(min_value=5, max_value=20, initial=8, label='Password length')
     
+
+class LoginForm(LoginView):
+    
+    class Meta:
+        model = User
+        fields = ['username', 'password',]
+
+
+class UserRegForm(UserCreationForm):
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
